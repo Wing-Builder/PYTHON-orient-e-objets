8098f0c316a8e2b968c01963528cbd9298a06053
Wing-Builder <marctsilavo@gmail.com>
2021-04-02 11:51:32 +0200

    Class Potion

---
+++ potion.py
@@ -0,0 +1,23 @@
+class Potion:
+    def __str__(self):
+        return self.name
+
+    def __init__(self, attributs):
+
+        if attributs = "Vie":
+            self.name = "regeneration"
+            self.type = "Potion de vie"
+            self.puissance = 100
+            self.utility_num = 10
+
+        if attributs = "Force":
+            self.name = "force"
+            self.type = "Potion de force"
+            self.puissance = 100
+            self.utility_num = 10
+
+        if attributs = "Defense":
+            self.name = "defense"
+            self.type = "Potion de defense"
+            self.puissance = 100
+            self.utility_num = 10
